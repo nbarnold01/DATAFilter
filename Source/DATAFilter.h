b10b14f3efb137346d8907bf7bf4f6216c1404ad
Nathan Arnold <nbarnold01@gmail.com>
2015-09-02 12:59:54 +0700

    Added methods to handle untouched objects.

--- Source/DATAFilter.h
+++ Source/DATAFilter.h
@@ -19,4 +19,24 @@
        inserted:(void (^)(NSDictionary *objectJSON))inserted
         updated:(void (^)(NSDictionary *objectJSON, NSManagedObject *updatedObject))updated;
 
++ (void)changes:(NSArray *)changes
+  inEntityNamed:(NSString *)entityName
+       localKey:(NSString *)localKey
+      remoteKey:(NSString *)remoteKey
+        context:(NSManagedObjectContext *)context
+       inserted:(void (^)(NSDictionary *objectJSON))inserted
+        updated:(void (^)(NSDictionary *objectJSON, NSManagedObject *updatedObject))updated
+      untouched:(void (^)( NSManagedObject *updatedObject))untouched;
+
++ (void)changes:(NSArray *)changes
+  inEntityNamed:(NSString *)entityName
+       localKey:(NSString *)localKey
+      remoteKey:(NSString *)remoteKey
+        context:(NSManagedObjectContext *)context
+      predicate:(NSPredicate *)predicate
+       inserted:(void (^)(NSDictionary *objectJSON))inserted
+        updated:(void (^)(NSDictionary *objectJSON, NSManagedObject *updatedObject))updated
+      untouched:(void (^)( NSManagedObject *updatedObject))untouched;
+
+
 @end
